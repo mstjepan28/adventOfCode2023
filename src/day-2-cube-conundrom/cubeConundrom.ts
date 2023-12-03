995cbef4fe272d82d6f617734862985bd1d73cef
Stjepan Markovčić <stjepan.markovcic@superius.hr>
2023-12-03 11:01:28 +0100

    day2 part2

--- src/day-2-cube-conundrom/cubeConundrom.ts
+++ src/day-2-cube-conundrom/cubeConundrom.ts
@@ -16,6 +16,7 @@ const extractNumber = (dirtyId: string) => {
   return Number(dirtyId.replace(/\D/g, ""));
 };
 
+// part 1
 const checkIfGameValid = (subsetStr: string) => {
   const subsetList = subsetStr.split(";");
 
@@ -35,6 +36,44 @@ const checkIfGameValid = (subsetStr: string) => {
   return true;
 };
 
+// part 2
+const extractColor = (value: string) => {
+  if (value.includes(RED)) {
+    return RED;
+  } else if (value.includes(BLUE)) {
+    return BLUE;
+  } else if (value.includes(GREEN)) {
+    return GREEN;
+  }
+
+  return undefined;
+};
+
+const getMaxColors = (subsetStr: string) => {
+  const colorValues = {
+    [RED]: 0,
+    [BLUE]: 0,
+    [GREEN]: 0,
+  };
+
+  const cubeValueList = subsetStr.replaceAll(";", ",").split(",");
+  for (const value of cubeValueList) {
+    const color = extractColor(value);
+    if (!color) {
+      continue;
+    }
+
+    const num = extractNumber(value);
+    const curMax = colorValues[color];
+
+    if (num > curMax) {
+      colorValues[color] = num;
+    }
+  }
+
+  return Object.values(colorValues).reduce((res, val) => res * val, 1);
+};
+
 const test = [
   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green, ",
   "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
@@ -44,17 +83,24 @@ const test = [
 ];
 
 export const cubeConundrum = () => {
-  const inputList = true ? test : puzzleInput;
+  const inputList = false ? test : puzzleInput;
 
-  const res = inputList.reduce((sum, game) => {
+  const part1Res = puzzleInput.reduce((sum, game) => {
     const [dirtyId, subsetStr] = game.split(":");
 
     const gameId = extractNumber(dirtyId);
     const isGameValid = checkIfGameValid(subsetStr);
 
-    console.log(gameId, subsetStr, isGameValid);
     return sum + (isGameValid ? gameId : 0);
   }, 0);
 
-  console.log("Result: ", res);
+  const part2Res = inputList.reduce((sum, game) => {
+    const subsetStr = game.split(":")[1];
+    const res = getMaxColors(subsetStr);
+
+    return sum + res;
+  }, 0);
+
+  console.log("Part 1 result: ", part1Res);
+  console.log("Part 2 result: ", part2Res);
 };
